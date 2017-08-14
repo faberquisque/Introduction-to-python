33b9183a6d37bde3442a23725bf16e59bcaff55f
Gastón Michel <michel.z.gaston@gmail.com>
2017-08-14 17:07:32 -0300

    Modificacion para cumplir con la consigna de presentacion

--- Michel_02.py
+++ Michel_02.py
@@ -3,29 +3,41 @@
 entrega-clase-2-GM.py
 """
 import math
+
+#imprimir nombre y apellido:
+print("Gaston Michel")
+print("Ejercicio 4")
+
 """
 Cuente la cantidad de veces que aparece un substring en un dado string. 
 Hágalo en los dos casos: distinguiendo entre mayúsculas y minúsculas, y 
 no distinguiendo. Realícelo, buscando los substrings es, la, que, co, en 
 el string. Cuente cuántas palabras tiene el string.
 """
-s = "Aquí me pongo a cantar Al compás de la vigüela, Que el hombre que lo desvela Una pena estraordinaria Como la ave solitaria Con el cantar se consuela."
+s = "Aquí me pongo a cantar\nAl compás de la vigüela,\nQue el hombre que lo desvela\nUna pena estraordinaria\nComo la ave solitaria\nCon el cantar se consuela."
 
 #print(s)
 m=["es","la","que","co"]
 
 # se cuentan las apariciones de los substrings en el string s, case-sensitive y case-insensitive
+print("Distinguiendo:",end=' ')
 for j in m:
-    print(j,'->',"sensitive",s.count(j),"insensitive",s.lower().count(j))
-    
+    print(s.count(j),end=' ')
+
+print()
+print("Sin distinguir:",end=' ') 
+for j in m:
+    print(s.lower().count(j),end=' ')
+
+print()
 # se cuentan las palabras contando los espacios + 1 
-print("Cantidad de palabras en s:",s.count(" ")+1)
+#print("Cantidad de palabras en s:",s.count(" ")+1)
 
 """
 Forme un nuevo string de 10 caracteres que contenga los 5 primeros y los 5 
 últimos del string anterior s.
 """
-print("Los 5 primero y los 5 ultimos caracteres:",s[:5]+s[-5:])
+print(s[:5]+s[-5:])
 
 """
 Forme un nuevo string que contenga los 10 caracteres centrales de s 
@@ -33,18 +45,18 @@ Forme un nuevo string que contenga los 10 caracteres centrales de s
 """
 n=10
 z=(len(s)-n)/2
-extracto=s[math.floor(z):-math.ceil(z)]
-print('Los',len(extracto),'caracteres centrales:',extracto)
+extracto=s[math.floor(z):-math.floor(z)]
+print(extracto)
 
 """
-Cambie todas las letras "a" por "e" y todas las letras "e" por "a" 
+Cambie todas las letras "m" por "n" y todas las letras "n" por "n" 
 en el string s
 """
 S=list(s)
 for i in range(len(S)):
-    if S[i]=='a':
-        S[i]='e'
-    elif S[i]=='e':
-        S[i]='a'
-print('Nuevo string con los caracteres a y e intercambiados')
+    if S[i]=='n':
+        S[i]='m'
+    elif S[i]=='m':
+        S[i]='n'
+
 print(''.join(S))
