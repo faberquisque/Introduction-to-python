4bcd5375110c06c6554d324e12906bdd1f4bfce1
Gastón Michel <michel.z.gaston@gmail.com>
2017-08-16 00:11:55 -0300

    creacion archivo de ejercicios restantes de la clase 2

---
+++ Ejercicios-02.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+'''5. Manejos de listas:'''
+''' 5.1. Cree la lista N de longitud 11, donde cada elemento es un número 
+entero de 0 a 10 (Ayuda: vea la expresión range). '''
+l=11
+N=list(range(l))
+print(N)
+'''5.2. Invierta la lista.'''
+print(N.reverse())
+'''5.3. Extraiga una lista N2 que contenga sólo los elementos pares de N.'''
