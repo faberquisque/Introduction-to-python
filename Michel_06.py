25d16f4f62c87c2ba706338dc2d4c2225c2ee066
Gastón Michel <michel.z.gaston@gmail.com>
2017-08-29 22:18:24 -0300

    entrega clase 6 casi terminada
    falla el parse en '- 3'. voy a armar otra rama para solucionar y optimizar codigo

--- Michel_06.py
+++ Michel_06.py
@@ -10,6 +10,7 @@ class Polinomio:
     else:
       print('ERROR: polinomio de grado mayor que 9')
     return None
+  
 
   def set_coeficientes(self, coefs=[]):
     """Si los coeficientes son proporcionados lo inicializa
@@ -34,7 +35,8 @@ class Polinomio:
     Keyword Arguments:
     p1 -- Polinomio a sumar
     """
-    return Polinomio([x+y for x,y in itertools.zip_longest(self.coeficientes, p1.coeficientes, fillvalue=0)])
+    from itertools import zip_longest
+    return Polinomio([x+y for x,y in zip_longest(self.coeficientes, p1.coeficientes, fillvalue=0)])
 
   def derivada(self, n=1):
     """Devuelve la derivada (n-ésima) del polinomio (un nuevo polinomio)
@@ -42,7 +44,7 @@ class Polinomio:
     n -- (default 1) Orden de derivación
 
     Modo de uso:
-    >>> P = Polinomio([0.1,2,3,0,1)
+    >>> P = Polinomio([0.1,2,3,0,1])
     >>> P1 = P.derivada()
     >>> P2 = P.derivada(n=2)
     """
@@ -98,25 +100,69 @@ class Polinomio:
 
     Modo de uso:
     >>> P = Polinomio()
-    >>> P.from_string('x + 2 x^2 + 3x + 1 + x^4', var='x')
+    >>> P.from_string('-x - 2 x^2 + 3x + 1 + x^4', var='x')
 
     No devuelve nada.
     Nota: Si una potencia aparece más de una vez, sus coeficientes se suman
     """
     sumas = s.split('+')
-    if len(sumas)==1:
-
+    if len(sumas) == 1:
+      restas = s.split('-')
+      if len(restas) == 1 or (len(restas)==2 and restas[0]==''):  #  si es un monomio o un binomio del estilo '0-a*x^n'
+        potencia = s.split('^')
+        if len(potencia) == 1:                        #sin potencia
+          constante = s.split(var)
+          a = constante[0].strip()
+          if len(constante) == 1:                         #sin x -> es una constante 'a'!
+            if a == '':
+              a = 0
+            self.coeficientes = [float(a)]                  # PARSE!!
+          elif constante[1].strip() == '':                #con x -> es un termino lineal 'a*x'!
+            if a == '': #'x'
+              a = 1
+            elif a == '-': #'-x'
+              a = -1
+            self.coeficientes = [0,float(a)]                # PARSE!!
+          else:
+            print('ERROR: mas de una x por termino')      #ERROR 'x'
+        elif len(potencia) == 2:                       #con potencia
+          n = int(potencia[1])
+          constante = potencia[0].split(var)
+          a = constante[0].strip()
+          if a == '':
+            a = 1
+          elif a == '-':
+            a = -1
+          if len(constante) == 1:                         #sin x-> Error!!
+            print('ERROR: no hay x en un termino con ^')
+          elif constante[1].strip() == '':                #con x -> es un termino 'a*x^n'
+            self.coeficientes = [0]*n+[float(a)]            # PARSE!!
+          else:
+            print('ERROR: mas de una x por termino')      #ERROR 'xx'
+        else:                                         #ERROR '^^^'
+          print('ERROR: mas de un ^ por termino')
+      else:
+        for i,a in enumerate(restas):
+          A = Polinomio()
+          if i == 0:
+            A.from_string(a,var)              # el primero suma ( si empieza con resta '-1-x' se toma '0-1-x')
+          else:
+            A.from_string('-'+a,var)          # los siguientes restan
+          self.coeficientes = self.suma_pol(A).coeficientes
+    else:
+      for a in sumas:
+        A = Polinomio()
+        A.from_string(a,var)
+        self.coeficientes = self.suma_pol(A).coeficientes  # se suman termino a termino y se vuelve recursiva hasta encontrar un monomio
 
 
 if __name__ == '__main__':
 
   print('Gaston Michel')
 
-  P1 = Polinomio([1, 2.1, 3, 1.])   # 1 + 2.1x + 3 x^2 + x^4
+  P1 = Polinomio([1, 2.1, 3, 1.])   # 1 + 2.1x + 3 x^2 + x^3
   P2 = Polinomio([0, 1, 0, -1, -2])  # x - x^3 - 2x^4
-  der=P1.integrada(5,(1,1,1,1,1))
-  print(P1)
-'''
+
   print(P1.get_coeficientes())
   print(P1.suma_pol(P2).get_coeficientes())
   P11 = P1.derivada()
@@ -130,4 +176,3 @@ if __name__ == '__main__':
   P3 = Polinomio()
   P3.from_string('x + 1 - x^2 - 3x^3')
   print(P3.get_coeficientes())
-  '''
